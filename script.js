ee912c60abcdf88786a4af6838882c6bc2e8a07c
Jessiiicaa <jessiicaabzz@gmail.com>
2025-02-09 21:18:01 -0300

    window.onload

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ document.getElementById("btn").addEventListener("click", function() {
     alert("Botão clicado! Bem-vindo ao seu site de teste.");
 });
 
+window.onload = function() {
+    // Verifique se há algum código de manipulação de botões aqui
+    document.getElementById("btn").addEventListener("click", function() {
+        alert("Botão clicado!");
+    });
+};
+
