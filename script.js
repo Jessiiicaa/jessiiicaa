8c444c8b8a756e9fd0bc52e582b08fa3d6ab3858
Jessiiicaa <jessiicaabzz@gmail.com>
2025-02-13 23:29:33 -0300

    ajustes

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ window.onload = function() {
 document.getElementById("botao5").addEventListener("click", function() {
     window.open("amovc.html", "_blank");
 });
+function appendToDisplay(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = '';
+}
+
+function calculateResult() {
+    const display = document.getElementById("display");
+    try {
+        display.value = eval(display.value); // Avalia a expressão matemática
+    } catch (error) {
+        display.value = 'Erro';
+    }
+}
